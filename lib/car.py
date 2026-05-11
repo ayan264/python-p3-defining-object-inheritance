c454595fd75eaeafa45b58b0ba314a30da63adb8
ayan <ayansaneyabdi@gmail.com>
2026-05-11 13:06:17 +0300

    completed the defening-object-inheritance and pass the tests

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,6 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        # Overriding the base class method to meet the TestCar requirements
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
